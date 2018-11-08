faba63e6b9411ad90e957ee56b7bbabc4adfe4a9
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-11-08 11:06:53 +0000

    Add .mli for Cmt2annot

---
+++ typing/cmt2annot.mli
@@ -0,0 +1,16 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                   Fabrice Le Fessant, INRIA Saclay                     *)
+(*                                                                        *)
+(*   Copyright 2018 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+val iterator: scope:Location.t -> bool -> Tast_mapper.mapper
