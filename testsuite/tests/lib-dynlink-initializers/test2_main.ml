75f71ba77a60aca8aefbf4705a6d04649664b9c0
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-11-09 10:04:26 +0000

    Port lib-dynlink-initializers tests to ocamltest

--- testsuite/tests/lib-dynlink-initializers/test2_main.ml
+++ testsuite/tests/lib-dynlink-initializers/test2_main.ml
@@ -1,3 +1,44 @@
+(* TEST
+
+include dynlink
+
+files = "test2_plugin.ml test2_second_plugin.ml"
+
+libraries = ""
+
+* shared-libraries
+** setup-ocamlc.byte-build-env
+*** ocamlc.byte
+module = "test2_main.ml"
+*** ocamlc.byte
+module = "test2_plugin.ml"
+*** ocamlc.byte
+module = "test2_second_plugin.ml"
+*** ocamlc.byte
+program = "${test_build_directory}/test2.byte"
+libraries = "dynlink"
+all_modules = "test2_main.cmo"
+**** run
+
+** native-dynlink
+*** setup-ocamlopt.byte-build-env
+**** ocamlopt.byte
+module = "test2_main.ml"
+**** ocamlopt.byte
+program = "test2_plugin.cmxs"
+flags = "-shared"
+all_modules = "test2_plugin.ml"
+**** ocamlopt.byte
+program = "test2_second_plugin.cmxs"
+flags = "-shared"
+all_modules = "test2_second_plugin.ml"
+**** ocamlopt.byte
+program = "${test_build_directory}/test2.exe"
+libraries = "dynlink"
+all_modules = "test2_main.cmx"
+***** run
+*)
+
 (* Check that a module in a loaded shared library whose initializer has not
    executed completely cannot be depended upon by another shared library being
    loaded. *)
