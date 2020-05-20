99cc9e64e3219d52d5a662dd2ae6ac239b8c1359
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-05-20 11:51:47 +0200

    Fix tests

--- testsuite/tests/utils/test_strongly_connected_components.ml
+++ testsuite/tests/utils/test_strongly_connected_components.ml
@@ -2,7 +2,7 @@
 include config
 include testing
 binary_modules =
-  "config build_path_prefix_map misc identifiable numbers \
+  "config build_path_prefix_map targetint misc identifiable numbers \
    strongly_connected_components"
 * bytecode
 *)
