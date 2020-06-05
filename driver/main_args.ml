32346abff2f9d1ffa9ced265f4ce1e7924b1e2b5
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-06-05 21:21:59 +0200

    Add -inc command-line flag

--- driver/main_args.ml
+++ driver/main_args.ml
@@ -144,6 +144,10 @@ let mk_I f =
   "-I", Arg.String f, "<dir>  Add <dir> to the list of include directories"
 ;;
 
+let mk_inc f =
+  "-inc", Arg.String f, "<file>  Add <file> to the load path"
+;;
+
 let mk_impl f =
   "-impl", Arg.String f, "<file>  Compile <file> as a .ml file"
 ;;
@@ -890,6 +894,7 @@ module type Common_options = sig
   val _absname : unit -> unit
   val _alert : string -> unit
   val _I : string -> unit
+  val _inc : string -> unit
   val _labels : unit -> unit
   val _alias_deps : unit -> unit
   val _no_alias_deps : unit -> unit
@@ -1156,6 +1161,7 @@ struct
     mk_stop_after ~native:false F._stop_after;
     mk_i F._i;
     mk_I F._I;
+    mk_inc F._inc;
     mk_impl F._impl;
     mk_intf F._intf;
     mk_intf_suffix F._intf_suffix;
@@ -1249,6 +1255,7 @@ struct
     mk_absname F._absname;
     mk_alert F._alert;
     mk_I F._I;
+    mk_inc F._inc;
     mk_init F._init;
     mk_labels F._labels;
     mk_alias_deps F._alias_deps;
@@ -1335,6 +1342,7 @@ struct
     mk_stop_after ~native:true F._stop_after;
     mk_i F._i;
     mk_I F._I;
+    mk_inc F._inc;
     mk_impl F._impl;
     mk_inline F._inline;
     mk_inline_toplevel F._inline_toplevel;
@@ -1469,6 +1477,7 @@ module Make_opttop_options (F : Opttop_options) = struct
     mk_alert F._alert;
     mk_compact F._compact;
     mk_I F._I;
+    mk_inc F._inc;
     mk_init F._init;
     mk_inline F._inline;
     mk_inline_toplevel F._inline_toplevel;
@@ -1569,6 +1578,7 @@ struct
     mk_absname F._absname;
     mk_alert F._alert;
     mk_I F._I;
+    mk_inc F._inc;
     mk_impl F._impl;
     mk_intf F._intf;
     mk_intf_suffix F._intf_suffix;
@@ -1693,6 +1703,7 @@ module Default = struct
   module Core = struct
     include Common
     let _I dir = load_path := Load_path.add_dir !load_path dir
+    let _inc file = load_path := Load_path.add_file !load_path file
     let _color = Misc.set_or_ignore color_reader.parse color
     let _dlambda = set dump_lambda
     let _dparsetree = set dump_parsetree
@@ -1933,6 +1944,7 @@ module Default = struct
       (* placeholder:
          Odoc_global.include_dirs := (s :: (!Odoc_global.include_dirs))
       *) ()
+    let _inc(_:string) = ()
     let _impl (_:string) =
       (* placeholder:
          Odoc_global.files := ((!Odoc_global.files) @ [Odoc_global.Impl_file s])
