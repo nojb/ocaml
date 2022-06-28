09255b0ae413a55ba8fac641847fe69d67fad5bb
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2022-06-28 22:11:42 +0200

    List.append, ( @ ) TRMC

--- stdlib/listLabels.mli
+++ stdlib/listLabels.mli
@@ -97,15 +97,11 @@ val init : len:int -> f:(int -> 'a) -> 'a list
  *)
 
 val append : 'a list -> 'a list -> 'a list
-(** Concatenate two lists. Same function as the infix operator [@].
-   Not tail-recursive (length of the first argument). The [@]
-   operator is not tail-recursive either.
- *)
+(** Concatenate two lists. Same function as the infix operator [@]. *)
 
 val rev_append : 'a list -> 'a list -> 'a list
 (** [rev_append l1 l2] reverses [l1] and concatenates it with [l2].
-   This is equivalent to [(]{!rev}[ l1) @ l2], but [rev_append] is
-   tail-recursive and more efficient.
+   This is equivalent to [(]{!rev}[ l1) @ l2].
  *)
 
 val concat : 'a list list -> 'a list
--- stdlib/stdlib.ml
+++ stdlib/stdlib.ml
@@ -295,7 +295,7 @@ let float_of_string_opt s =
 
 (* List operations -- more in module List *)
 
-let rec ( @ ) l1 l2 =
+let[@tail_mod_cons] rec ( @ ) l1 l2 =
   match l1 with
     [] -> l2
   | hd :: tl -> hd :: (tl @ l2)
